1ddbb389ae241762400b2c7ecd7cbbce2bedeb5f
KillianDB <killiandomingues57@gmail.com>
2023-08-23 14:46:59 -0300

    setando porta da aplicacao e intalando dependencias iniciais do node no projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+//importando o express
+const express = require('express');
+//importando o path(da o caminho dos arquivos)
+const path = require('path');
+//executando o express
+const app = express();
+//salvando o numero da porta a ser usada numa variavel
+const PORT = 3000;
+
+//funcao get no express(app) para enviar a view(index.html) quando a porta padrao for acessada
+//(req=requisicao, res=resposta)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//funcao listen para rodar a aplicacao na porta indicada
+app.listen(PORT, () => {
+  console.log(`O servidor esta rodando na porta ${PORT}`);
+});
