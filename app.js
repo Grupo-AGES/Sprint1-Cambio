628dc06756b2bd0f88227e18a9ac61ccc3d51807
KillianDB <killiandomingues57@gmail.com>
2023-08-24 15:42:32 -0300

    setando a public como lugar padrao dos arquivos estaticos

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 //salvando o numero da porta a ser usada numa variavel
 const PORT = 3000;
 
+//funcao use, indicando para o express onde vao ficar os arquivos estaticos do projeto(importante para nao crashar o css e as imagens, o navegador pode ler como texto se nao tiver)
+app.use(express.static(path.join(__dirname, "public")));
 //funcao get no express(app) para enviar a view(index.html) quando a porta padrao for acessada
 //(req=requisicao, res=resposta)
 app.get('/', (req, res) => {
