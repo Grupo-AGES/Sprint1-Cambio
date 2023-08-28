e5a476f3e6aff329f21bbd288e74b3da9577b3ab
KillianDB <killiandomingues57@gmail.com>
2023-08-28 14:45:38 -0300

    arrumando bug valor do input e comentando script modo escuro

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,16 @@
+//pega botao para modo escuro
 const toggleButton = document.getElementById('toggle-mode');
+//pega body inteiro da pagina
 const body = document.body;
+//seleciona o input do valor dado ao cliente
+let valor = document.getElementById("valor");
+console.log(valor.value)
+//seleciona o primeiro select de moeda do html
+let coinSelect = document.getElementById("coinSelect");
+//seleciona o segundo select de moeda do html
+let coinInSelect = document.getElementById("coinInSelect");
        
+//altera o modo quando clicar no botao
     toggleButton.addEventListener('click', () => {
         if (body.classList.contains('dark-mode')) {
             body.classList.remove('dark-mode');
@@ -11,13 +21,7 @@ const body = document.body;
         }
     });
 
-//seleciona o input do valor dado ao cliente
-let valor = document.getElementById("valor");
-console.log(valor.value)
-//seleciona o primeiro select de moeda do html
-let coinSelect = document.getElementById("coinSelect");
-//seleciona o segundo select de moeda do html
-let coinInSelect = document.getElementById("coinInSelect");
+
 //salva os valores selecionados no select pelo usuario para encaminhar para api
 let moedasSelecionadas = coinSelect.value + "-" + coinInSelect.value;
 let apiUrl = `https://economia.awesomeapi.com.br/json/last/${moedasSelecionadas}`;
