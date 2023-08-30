519b2c7c76b4c32372fe85e9d07a0144c124e4d5
KillianDB <killiandomingues57@gmail.com>
2023-08-30 15:47:13 -0300

    mudando layout e onde renderiza resposta do calculo

--- public/script/scriptCoins.js
+++ public/script/scriptCoins.js
@@ -7,9 +7,6 @@ let coinSelect = document.getElementById("coinSelect");
 // Seleciona o segundo select de moeda do HTML
 let coinInSelect = document.getElementById("coinInSelect");
 
-//Seleciona o botao de converter do HTML
-let botaoC = document.getElementById("converter");
-
 //Seleciona div do resultado no HTML
 let resultadoDisplay = document.getElementById("resultadoDisplay");
 
@@ -22,25 +19,26 @@ function mudancaSelects() {
     const currencyPair = `${selectedCoin}-${selectedCoinIn}`;
     const apiUrl = `https://economia.awesomeapi.com.br/json/last/${currencyPair}`;
 
+
+    // Requisitando HTTP para usar axios
+    axios.get(apiUrl)
+        .then(response => {
+            const data = response.data;
+            for (const currencyPair in data) {
+                const cotacao = data[currencyPair]['bid'];
+                const dia = data[currencyPair]['create_date'];
+                const resultado = cotacao * valorInput;
+
+                resultadoDisplay.value = `${resultado}`;
+                console.log(`Currency Pair: ${currencyPair}, Cotacao: ${cotacao}, Dia: ${dia}`);
+                console.log("O resultado: " + resultado);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        })
+};
+
+valor.addEventListener('input', mudancaSelects);
 coinSelect.addEventListener('change', mudancaSelects);
 coinInSelect.addEventListener('change', mudancaSelects);
-
-// Requisitando HTTP para usar axios
-axios.get(apiUrl)
-    .then(response => {
-        const data = response.data;
-        for (const currencyPair in data) {
-            const cotacao = data[currencyPair]['bid'];
-            const dia = data[currencyPair]['create_date'];
-            const resultado =cotacao*valorInput;
-
-            resultadoDisplay.textContent = `Par: ${currencyPair}, Cotacao: ${cotacao}, Data: ${dia}. Resultado: ${resultado}`;
-            console.log(`Currency Pair: ${currencyPair}, Cotacao: ${cotacao}, Dia: ${dia}`);
-            console.log("O resultado: "+ resultado);
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    })};
-
-    botaoC.addEventListener('click', mudancaSelects);
